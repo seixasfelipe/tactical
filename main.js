80330c3e016a23247dfca149f334c8f5e7d26505
Felipe Seixas <seixasfelipe@gmail.com>
2016-12-10 21:45:48 -0500

    grid init

---
+++ main.js
@@ -0,0 +1,234 @@
+var TACTICAL = (function(o) {
+
+    var canvas
+
+    var game = {
+
+        ctx: null, 
+        WIDTH: 0, 
+        HEIGHT: 0, 
+        // ROWS: 0, 
+        // COLS: 0,
+        TILE_SIZE: 0,
+        HALF_PIXEL: 0.5,
+
+        unselectedTile: {
+            fillStyle: "rgb(255,255,255)",
+            strokeStyle: "rgb(0,0,0)"
+
+        },
+        selectedTile: {
+            fillStyle: "rgb(255,0,0)",
+            strokeStyle: "rgb(0,0,0)"
+        },
+        highlightedTile: {
+            fillStyle: "rgb(0,255,0)",
+            strokeStyle: "rgb(0,0,0)"
+        },
+
+        EventTypeEnum: Object.freeze({
+            CLICK: 1, 
+            MOVE: 2
+        }),
+
+        // map: [
+        //     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        //     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        //     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        //     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        //     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        //     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        //     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        //     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        //     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        //     0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        // ],
+
+        init: function(c) {
+
+            this.ctx = canvas.getContext('2d')
+
+            this.ctx.fillStyle = this.unselectedTile.fillStyle
+            this.ctx.fillRect(0, 0, c.width, c.height)
+
+            this.WIDTH   = c.width
+            this.HEIGHT  = c.height
+            // this.ROWS    = 10
+            // this.COLS    = 10
+            this.TILE_SIZE = 32
+
+            var that = this
+
+            c.onclick = function(e) {
+                that.handleEvent.call(that, that.EventTypeEnum.CLICK, { x: e.offsetX, y: e.offsetY })
+            }
+
+            c.onmousemove = function(e) {
+                that.handleEvent.call(that, that.EventTypeEnum.MOVE, { x: e.offsetX, y: e.offsetY })
+            }
+
+            this.draw()
+
+        },
+
+        draw: function() {
+
+            // var currentRow = 0, currentColumn = 0
+            
+            // for(var i=0; i < this.map.length; i += 1) {
+                
+                // Next row
+                // if ( currentRow != parseInt( (i / this.ROWS), 10) ) {
+                //     currentRow = parseInt( (i / this.ROWS), 10)
+                //     currentColumn = 0    
+                // }
+
+                // this.ctx.strokeRect(currentRow * this.TILE_SIZE, 
+                //     currentColumn * this.TILE_SIZE,
+                //     this.TILE_SIZE, 
+                //     this.TILE_SIZE)
+                
+                // currentColumn += 1
+
+            // }
+
+            // draw vertical lines
+            var i = 0,
+                rows = parseInt(this.HEIGHT / this.TILE_SIZE, 10),
+                cols = parseInt(this.WIDTH / this.TILE_SIZE, 10)
+
+                console.log(rows)
+                console.log(cols)
+            while(i <= cols) {
+
+                this.ctx.moveTo(i * this.TILE_SIZE + this.HALF_PIXEL, 0)
+                this.ctx.lineTo(i * this.TILE_SIZE + this.HALF_PIXEL, rows * this.TILE_SIZE)
+
+                i += 1
+
+            }
+
+            // draw horizontal lines
+            i = 0
+            while(i <= rows) {
+
+                this.ctx.moveTo(0, i * this.TILE_SIZE + this.HALF_PIXEL)
+                this.ctx.lineTo(cols * this.TILE_SIZE, i * this.TILE_SIZE + this.HALF_PIXEL)
+                
+                i += 1
+
+            }
+
+            this.ctx.stroke()
+
+        },
+
+        drawTile: function(row, col, fillStyle, strokeStyle) {
+
+            this.ctx.fillStyle = fillStyle
+            this.ctx.fillRect(row * this.TILE_SIZE + 1,
+                col * this.TILE_SIZE + 1,
+                this.TILE_SIZE - 1,
+                this.TILE_SIZE - 1)
+            
+            this.ctx.strokeStyle = strokeStyle
+            this.ctx.strokeRect(row * this.TILE_SIZE + this.HALF_PIXEL,
+                col * this.TILE_SIZE + this.HALF_PIXEL,
+                this.TILE_SIZE,
+                this.TILE_SIZE)
+
+        },
+
+        selectTile: function(e) {
+
+            var row = parseInt(e.x / this.TILE_SIZE, 10), 
+                col = parseInt(e.y / this.TILE_SIZE, 10)
+
+            console.log('selectedTile: ' + row + ', ' + col)
+
+            if(typeof this.selectedTile.row === "number") {
+
+                this.drawTile(this.selectedTile.row, this.selectedTile.col, 
+                    this.unselectedTile.fillStyle, this.unselectedTile.strokeStyle)
+
+            }
+
+            this.selectedTile.row = row
+            this.selectedTile.col = col
+
+            this.highlightedTile.row = null
+            this.highlightedTile.col = null
+
+            this.drawTile(this.selectedTile.row, this.selectedTile.col, 
+                this.selectedTile.fillStyle, this.selectedTile.strokeStyle)
+        
+        },
+
+        highlightTile: function(e) {
+
+            var row = parseInt(e.x / this.TILE_SIZE, 10), 
+                col = parseInt(e.y / this.TILE_SIZE, 10)
+
+            console.log('highlightedTile: ' + row + ', ' + col)
+
+            if(typeof this.highlightedTile.row === "number") {
+
+                this.drawTile(this.highlightedTile.row, this.highlightedTile.col, 
+                    this.unselectedTile.fillStyle, this.unselectedTile.strokeStyle)
+
+            }
+
+            if(typeof this.selectedTile.row === "number" &&
+                row === this.selectedTile.row && col === this.selectedTile.col) {
+                return;
+            }
+
+            this.highlightedTile.row = row
+            this.highlightedTile.col = col
+
+            this.drawTile(this.highlightedTile.row, this.highlightedTile.col, 
+                this.highlightedTile.fillStyle, this.highlightedTile.strokeStyle)
+        },
+
+        handleEvent: function(type, e) {
+
+            switch(type) {
+                
+                case this.EventTypeEnum.CLICK:
+                    this.selectTile(e)
+                    break;
+
+                case this.EventTypeEnum.MOVE:
+                    this.highlightTile(e)
+                    break;
+                
+                default:
+                    break;
+            }
+
+        }
+
+    }
+
+
+    var init = function() {
+        
+        canvas = document.createElement('canvas')
+        canvas.width = 640
+        canvas.height = 480
+        canvas.id = 'canvas2d'
+
+        if(document.getElementById('canvas2d') == null) {
+            document.body.appendChild(canvas)
+
+            game.init(canvas)
+        }
+
+    }
+
+    
+    init()
+
+    return o
+
+}(TACTICAL || {}))
